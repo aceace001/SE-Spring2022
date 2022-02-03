35e963a87f7f9686360aa718c70d4e0d88df6075
aceace001 <72894562+aceace001@users.noreply.github.com>
2022-02-03 08:08:01 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,63 +1,40 @@
-import {useState} from 'react'
-import Header from "./components/Header"
-import Tasks from "./components/Tasks"
-import AddTask from './components/AddTask'
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import Home from "./page/Home";
+import Login from "./page/Login";
+import Register from "./page/Register";
+import TaskBar from "./TaskBar/TaskBar";
+import TaskBar1 from "./TaskBar/TaskBar1";
+import HomePost from "./page/HomePost";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-const App = () => {
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: 'Doctors Appointment',
-      day: 'Feb 5th at 2: 30pm',
-      reminder: true, 
-    },
-    {
-      id: 2,
-      text: 'Meeting at School',
-      day: 'Fed 6th at 1:30pm',
-      reminder: true,
-    },
-    {
-      id: 3,
-      text: 'Food Shopping',
-      day: 'Feb 5th at 2:30pm',
-      reminder: false,
-    },
-  ])
-
-  //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random()*10000) + 1
-    const newTask = {id, ...task}
-    setTasks([...tasks, newTask])
-  }
-
-  //Delete Task
-  const deleteTask = (id) =>{
-    setTasks(tasks.filter((task)=>task.id !== id))
-  }
-
-  //Toggle Reminder
-  const toggleReminder = (id) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === id ? {...task, reminder:!task.reminder} : task
-      )
-    )
-  }
-
+function App() {
+  const [name, setName] = useState('');
   return (
-    <div className="container">
-      <Header onAdd={()=>setShowAddTask(!showAddTask)} showAdd={showAddTask} />
-      {showAddTask && <AddTask onAdd={addTask}/>}
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
-      onToggle={toggleReminder}/>:'No Tasks Found'}
-    </div>
-  )
+      <div className="App">
+        <BrowserRouter>
+          <TaskBar name={name} setName={setName}/>
+          <div className={"home"}>
+              <Routes>
+                  <Route path="/" element={<Home/>}/>
+              </Routes>
+
+          </div>
+          <main className="form-signin">
+              <Routes>
+                  <Route path="/Login" element={<Login/>}/>
+                  <Route path="/Register" element={<Register/>}/>
+              </Routes>
+          </main>
+          <div className={"homepost"}>
+              <Routes>
+                  <Route path="/HomePost" element={<HomePost/>}/>
+              </Routes>
+          </div>
+        </BrowserRouter>
+      </div>
+  );
 }
 
-
-
-
 export default App;
+
