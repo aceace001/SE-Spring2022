92a13d2686a2c3d7aeccfa6a92e2810c9c19a535
Fugang Deng <fugang625@gmail.com>
2022-02-14 18:49:08 -0500

    The main file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Taskbar from './Components/Taskbar'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Report from './pages/Report'
+import Product from './pages/Product'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Taskbar />
+      <Switch>
+        <Route path='/' exact component={Home}/> //We should have exact to make the page render
+        <Route path='/reports' component={Report}/>
+        <Route path='/products' component={Product}/>
+      </Switch>
+    </Router>
+    
+    </>
   );
 }
 
