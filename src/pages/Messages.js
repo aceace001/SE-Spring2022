44bd881241abfcd2cbbd6c639f52edceb34a4bf0
Fugang Deng <fugang625@gmail.com>
2022-02-16 15:43:23 -0500

    Add the messages page

---
+++ src/pages/Messages.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Messages() {
+  return (
+    <div className='messages'>
+        <h1>This is the page for Messages </h1>
+      
+    </div>
+  )
+}
+
+export default Messages
