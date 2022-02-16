dccc8aab79cd342f76a48066e45dce6bca60af52
Fugang Deng <fugang625@gmail.com>
2022-02-16 15:52:55 -0500

    Fixed an small error to show correct styling

--- src/Components/Taskbar.js
+++ src/Components/Taskbar.js
@@ -30,7 +30,7 @@ function Taskbar() {
                 </li>
                 {SidebarData.map((item,index) =>{
                     return(
-                        <li key={index} className={item.Cname}>
+                        <li key={index} className={item.cName}>
                             <Link to={item.path}>
                                 {item.icon}
                                 <span>{item.title}</span>
