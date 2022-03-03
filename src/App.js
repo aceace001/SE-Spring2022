c6f4bc3eb1b2c6b82e1bbcc37e16ac77e7401644
Fugang Deng <fugang625@gmail.com>
2022-03-02 22:31:45 -0500

    update code with the combination of branch frontend#1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+
 import './App.css'
 import Taskbar from './Components/Taskbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import Report from './pages/Report'
-import Product from './pages/Product'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Search from './pages/Search'
+import Addfriends from './pages/Addfriends'
+import Posts from './pages/Posts'
+import Support from './pages/Support'
+import Messages from './pages/Messages'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import HomePost from './pages/HomePost'
+
 
 
 function App() {
   return (
     <>
+    
     <Router>
       <Taskbar />
       <Routes>
-        <Route path='/' element = {<Home />}/> 
-        <Route path='/reports' element={<Report />}/>
-        <Route path='/products' element={<Product />}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/HomePost' element={<HomePost />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/settings' element={<Settings />}/>
+        <Route path='/users' element={<Users />}/>
+        <Route path='/messages' element={<Messages />}/>
+        <Route path='/search' element={<Search />}/>
+        <Route path='/addfriends' element={<Addfriends />}/>
+        <Route path='/posts' element={<Posts />}/>
+        <Route path='/support' element={<Support />}/>
       </Routes>
     </Router>
     
