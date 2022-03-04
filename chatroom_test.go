6e97cac0c7d29991e2c52a8aa3a9c6922d43b45f
Yuhan Jin <57483688+rookiefromgis@users.noreply.github.com>
2022-03-05 03:19:48 +0800

    Update chatroom_test.go

--- chatroom_test.go
+++ chatroom_test.go
@@ -4,26 +4,50 @@ import (
 	"testing"
 )
 
-//login and register
-func TestLogin(t *testing.T) {
+// //login and register
+// func TestLogin(t *testing.T) {
 
-	var (
-		idIn   string
-		nameIn string
-	)
+// 	var (
+// 		idIn   string
+// 		nameIn string
+// 	)
 
-	idIn = "inputId"
-	nameIn = "inputName"
+// 	idIn = "inputId"
+// 	nameIn = "inputName"
 
-	ts := &TestStruct{}
-	idOut, nameOut := ts.StructFunc(idIn, nameIn)
+// 	ts := &TestStruct{}
+// 	idOut, nameOut := ts.StructFunc(idIn, nameIn)
 
-	if idOut == idIn && nameOut == nameIn {
-		t.Log("Test passed！")
-	} else {
-		t.Error("function execution error")
+// 	if idOut == idIn && nameOut == nameIn {
+// 		t.Log("Test passed！")
+// 	} else {
+// 		t.Error("function execution error")
+// 	}
+
+// }
+
+//test register
+func TestRegister(t *testing.T) {
+
+	var jsonStr = []byte(`{"id":4,"first_name":"xyz","last_name":"pqr","email":"xyz@pqr.com","password":"1234567890"}`)
+
+	req, err := http.NewRequest("POST", "/api/register", bytes.NewBuffer(jsonStr)) //router.POST("/api/register", users.Register)
+	if err != nil {
+		t.Fatal(err)
 	}
-
+	req.Header.Set("Content-Type", "application/json")
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(Reister)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+	expected := `{"id":4,"first_name":"xyz","last_name":"pqr","email_address":"xyz@pqr.com","phone_number":"1234567890"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
 }
 
 //User online broadcast
