4faebdd7873c478e2d2fbde10b74709974c4b1b5
Yuhan Jin <57483688+rookiefromgis@users.noreply.github.com>
2022-03-05 04:03:30 +0800

    Update chatroom_test.go

--- chatroom_test.go
+++ chatroom_test.go
@@ -10,7 +10,22 @@ import (
 )
 //inital db
 
-// //login and register
+//login
+// func TestUserLogin(t *testing.T) {
+// 	email := "youngxhui@163.com"
+// 	value := url.Values{}
+// 	value.Add("email", email)
+// 	value.Add("password", "1234")
+// 	w := httptest.NewRecorder()
+// 	req, _ := http.NewRequest(http.MethodPost, "/user/login", bytes.NewBufferString(value.Encode()))
+// 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded; param=value")
+// 	router.ServeHTTP(w, req)
+// 	assert.Equal(t, http.StatusOK, w.Code)
+// 	assert.Equal(t, strings.Contains(w.Body.String(), email), true)
+// }
+
+
+
 // func TestLogin(t *testing.T) {
 
 // 	var (
