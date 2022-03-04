4280ce4c16fddf63c4a9e4ca7eb2ad5cb901587b
Yuhan Jin <57483688+rookiefromgis@users.noreply.github.com>
2022-03-05 04:08:26 +0800

    Update users_dto.go

--- backend/domain/users/users_dto.go
+++ backend/domain/users/users_dto.go
@@ -7,15 +7,11 @@ import (
 
 type User struct {
 	ID        int64  `json:"ID"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
 	Password  string `json:"password"`
 	Email     string `json:"email"`
 }
 
 func (user *User) Validate() *errors.RestErr {
-	user.FirstName = strings.TrimSpace(user.FirstName)
-	user.LastName = strings.TrimSpace(user.LastName)
 	user.Email = strings.TrimSpace(user.Email)
 	if user.Email == "" {
 		return errors.NewBadRequestError("invalid email address")
