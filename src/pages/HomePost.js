15bceb6eb793893d5fc9fc43b3d87e52722ccc2e
aceace001 <72894562+aceace001@users.noreply.github.com>
2022-03-04 01:16:05 -0500

    Update HomePost.js

--- src/pages/HomePost.js
+++ src/pages/HomePost.js
@@ -1,10 +1,28 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import TaskBar1 from "../Components/Taskbar1";
 const HomePost = () => {
     const [name, setName] = useState('');
-    return (
-        <TaskBar1 name={name} setName={setName}/>
-    );
-};
 
+
+    useEffect(() => {
+        (
+            async () => {
+                const response = await fetch('http://localhost:8081/api/user', {
+                    headers: {'Content-Type': 'application/json'},
+                    credentials: 'include',
+                });
+
+                const content = await response.json();
+
+                setName(content.name);
+            }
+        )();
+    });
+
+    return (
+        <div>
+            {name ? "Welcome" + name :"Wrong username or password"}
+        </div>
+    )
+}
 export default HomePost;
