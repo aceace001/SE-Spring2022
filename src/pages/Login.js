79a6fd283bdc9e9a38244c40a095cfa7ebfd9f3e
aceace001 <72894562+aceace001@users.noreply.github.com>
2022-03-04 01:14:49 -0500

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -10,24 +10,33 @@ const Login = (props: { setName: (name: string) => void }) => {
     const submit = async (e: SyntheticEvent) => {
         e.preventDefault();
 
+        await fetch('http://localhost:8081/api/login',{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            credentials: 'include',
+            body: JSON.stringify({
+                email,
+                password
+            })
+        })
         setNavi(true);
     }
 
     if (navi) {
-        return <Navigate to="/HomePost"/>;
+        return <Navigate to="/Homepost"/>;
     }
 
     return (
         <div className="login">
         <form className={"p-4 p-md-5 border rounded-3 bg-light"} onSubmit={submit}>
             <h1 className="h3 mb-3 fw-normal">Please login</h1>
-            <div className={"form-floating mb-3"} id='email'>
+            <div className={"form-floating mb-3"}>
                 <input type="email" className="form-control" placeholder="Email address" required
                        onChange={e => setEmail(e.target.value)}
                 />
                 <label>Email Address</label>
             </div>
-            <div className={"form-floating mb-3"} id='password'>
+            <div className={"form-floating mb-3"}>
                 <input type="password" className="form-control" placeholder="Password" required
                        onChange={e => setPassword(e.target.value)}
                 />
