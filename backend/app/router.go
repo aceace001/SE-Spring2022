a991b60fe360680c2d053e0882a55edcd350cee9
Dafei Du <dud@oregonstate.edu>
2022-03-04 10:10:58 -0500

    combine sprint1

---
+++ backend/app/router.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"main/controller/users"
+	"time"
+
+	"github.com/gin-contrib/cors"
+)
+
+func mapUrls() {
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://localhost:3000"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+	router.POST("/api/register", users.Register)
+	router.POST("/api/login", users.Login)
+	router.GET("/api/user", users.Get)
+	router.GET("/api/logout", users.Logout)
+	router.GET("/api/email", users.Email)
+}
---
+++ backend/domain/users/users_dao.go
@@ -0,0 +1,60 @@
+package users
+
+import (
+	"main/datasource/mysql/users_db"
+	"main/utils/errors"
+)
+
+var (
+	queryInsertUser     = "INSERT INTO users (first_name, last_name, email, password) VALUES (?, ?, ?, ?);"
+	queryGetUserByEmail = "SELECT id, first_name, last_name, email, password FROM users WHERE email=?;"
+	queryGetUserByID    = "SELECT id, first_name, last_name, email FROM users WHERE id=?;"
+)
+
+func (user *User) Save() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryInsertUser)
+	if err != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+	defer stmt.Close()
+
+	insertResult, saveErr := stmt.Exec(user.FirstName, user.LastName, user.Email, user.Password)
+	if saveErr != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+
+	userID, err := insertResult.LastInsertId()
+	if err != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+	user.ID = userID
+	return nil
+}
+
+func (user *User) GetByEmail() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryGetUserByEmail)
+	if err != nil {
+		return errors.NewInternalServerErrror("invalid email")
+	}
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.Email)
+	if getErr := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email, &user.Password); getErr != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+	return nil
+}
+
+func (user *User) GetByID() *errors.RestErr {
+	stmt, err := users_db.Client.Prepare(queryGetUserByID)
+	if err != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+	defer stmt.Close()
+
+	result := stmt.QueryRow(user.ID)
+	if getErr := result.Scan(&user.ID, &user.FirstName, &user.LastName, &user.Email); getErr != nil {
+		return errors.NewInternalServerErrror("database error")
+	}
+	return nil
+}
