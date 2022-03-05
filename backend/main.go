cdf2877557620ed1b6ebc9f55d6b5dd57243ac82
Dafei Du <dud@oregonstate.edu>
2022-03-04 20:48:35 -0500

    update

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,10 @@
 package main
 
-import "main/app"
-
 func main() {
 	// sprint 2( client & server)
-	// server := NewServer("127.0.0.1", 8081)
-	// server.Start()
+	server := NewServer("127.0.0.1", 8081)
+	server.Start()
 	//tcp
-	app.StartApplication()
+	// app.StartApplication()
 
 }
