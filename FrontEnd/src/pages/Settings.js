2dcadc6ddab06d0d8ae9d6f17dd09d3760918f87
Fugang Deng <fugang625@gmail.com>
2022-03-04 22:50:29 -0500

    Create The Ssttings.js

---
+++ FrontEnd/src/pages/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Settings() {
+  return (
+    <div className='settings'>
+        <h1>This is the page for settings </h1>
+      
+    </div>
+  )
+}
+
+export default Settings
