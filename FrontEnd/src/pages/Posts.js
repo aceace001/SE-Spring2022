0fdf92eba1d217fedbaba87c4799f37b7353acc2
Fugang Deng <fugang625@gmail.com>
2022-03-04 22:49:29 -0500

    Create The Posts.js

---
+++ FrontEnd/src/pages/Posts.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <div className='posts'>
+        <h1>This is the page for Posts </h1>
+      
+    </div>
+  )
+}
+
+export default Posts
