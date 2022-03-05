e6e22bcb14ebe743b41e4a66180b4a1ef6baa096
Dafei Du <dud@oregonstate.edu>
2022-03-04 20:40:28 -0500

    fix some bugs

--- backend/main.go
+++ backend/main.go
@@ -4,9 +4,9 @@ import "main/app"
 
 func main() {
 	// sprint 2( client & server)
-	server := NewServer("127.0.0.1", 8081)
-	server.Start()
-	//tcp 
+	// server := NewServer("127.0.0.1", 8081)
+	// server.Start()
+	//tcp
 	app.StartApplication()
-	
+
 }
