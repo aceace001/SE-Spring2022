b40dcd7c09ae1606f6986280cf7b93ffa00a1f5a
Fugang Deng <fugang625@gmail.com>
2022-03-04 22:48:53 -0500

    Create The Addfrineds.js

---
+++ FrontEnd/src/pages/Addfriends.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Addfriends() {
+  return (
+    <div className='addfriends'>
+        <h1>This is the page for Addfriends </h1>
+      
+    </div>
+  )
+}
+
+export default Addfriends
