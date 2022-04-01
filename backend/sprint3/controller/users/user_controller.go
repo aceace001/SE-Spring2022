cbbe5bd107fe5c97675bca765ff878eb2a484cf7
Dafei Du <dud@oregonstate.edu>
2022-04-01 19:51:18 -0400

    Create user_controller.go

---
+++ backend/sprint3/controller/users/user_controller.go
@@ -0,0 +1,137 @@
+package users
+
+import (
+	"fmt"
+	"io/ioutil"
+	"main/domain/users"
+	"main/services"
+	"main/utils/errors"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	SecretKey = "qwe123"
+)
+
+//  users register an account
+func Register(c *gin.Context) {
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		err := errors.NewBadRequestError("invald json body")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+// users login the account
+func Login(c *gin.Context) {
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		err := errors.NewBadRequestError("invalid json")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	result, getErr := services.GetUser(user)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(result.ID)),
+		ExpiresAt: time.Now().Add(time.Hour * 72).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(SecretKey))
+	if err != nil {
+		err := errors.NewInternalServerErrror("login failed")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	c.SetCookie("jwt", token, 3600, "/", "localhost", false, true)
+
+	c.JSON(http.StatusOK, result)
+}
+
+// get users' information
+func Get(c *gin.Context) {
+	cookie, err := c.Cookie("jwt")
+	if err != nil {
+		getErr := errors.NewInternalServerErrror("could not retrive cookie")
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(*jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		restErr := errors.NewInternalServerErrror("error parsing cookie")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+	claims := token.Claims.(*jwt.StandardClaims)
+	issuer, err := strconv.ParseInt(claims.Issuer, 10, 64)
+	if err != nil {
+		restErr := errors.NewBadRequestError("user id should be a number")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, restErr := services.GetUserByID(issuer)
+	if restErr != nil {
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, result)
+}
+
+// log out the account
+func Logout(c *gin.Context) {
+	c.SetCookie("jwt", "", -1, "", "", false, true)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "success",
+	})
+}
+
+// check if email exist or not
+func Email(c *gin.Context) {
+
+}
+
+// sprint3
+// create posts
+func HomePage(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Welcome to ...",
+	})
+}
+
+func PostHomePage(c *gin.Context) {
+	body := c.Request.Body
+	value, err := ioutil.ReadAll(body)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	c.JSON(200, gin.H{
+		"message": string(value),
+	})
+}
