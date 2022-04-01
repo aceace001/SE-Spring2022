3dabd74a8e4a39f1c2b600c2157d12e74703c0ae
Fugang Deng <fugang625@gmail.com>
2022-04-01 16:47:48 -0400

    The index file of Supoort Admin to do the chat API

---
+++ FrontEnd/src/pages/SupportAdmin/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
+const SupportAdmin = () =>{
+    return (
+        <ChatEngine
+            projectID={'298755fc-0e1a-4390-ab40-7134b4727cc0'}
+            userName='Fugang Deng'
+            userSecret='Fugang625!'
+            height='calc(100vh - 20px)'
+        />
+    )
+}
+
+export default SupportAdmin;
