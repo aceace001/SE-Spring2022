c11fe974e27874ae88b7dc1d14d6abaf6b19e37f
Yuhan Jin <57483688+rookiefromgis@users.noreply.github.com>
2022-04-20 02:46:21 +0800

    Update user_service.go

--- backend/sprint4/user_service.go
+++ backend/sprint4/user_service.go
@@ -1,1 +1,34 @@
+//service/user_service.go
+func (u *userService) AddFriend(userFriendRequest *request.FriendRequest) error {
+    var queryUser *model.User
+    db := pool.GetDB()//connect to database
+    db.First(&queryUser, "uuid = ?", userFriendRequest.Uuid)
+    log.Logger.Debug("queryUser", log.Any("queryUser", queryUser))
+    var nullId int32 = 0
+    if nullId == queryUser.Id {
+        return errors.New("用户不存在")
+    }
 
+    var friend *model.User
+    db.First(&friend, "username = ?", userFriendRequest.FriendUsername)
+    if nullId == friend.Id {
+        return errors.New("已添加该好友")
+    }
+
+    userFriend := model.UserFriend{
+        UserId:   queryUser.Id,
+        FriendId: friend.Id,
+    }
+
+    var userFriendQuery *model.UserFriend
+    db.First(&userFriendQuery, "user_id = ? and friend_id = ?", queryUser.Id, friend.Id)
+    if userFriendQuery.ID != nullId {
+        return errors.New("该用户已经是你好友")
+    }
+
+    db.AutoMigrate(&userFriend)
+    db.Save(&userFriend)
+    log.Logger.Debug("userFriend", log.Any("userFriend", userFriend))
+
+    return nil
+}
