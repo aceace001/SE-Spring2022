aa6c2da40e3f782db32806f34c4700dbbac1a2cb
Fugang Deng <49621024+TTUDFG@users.noreply.github.com>
2022-04-20 23:56:21 -0400

    Implment get to see frinds with mock data

--- FrontEnd/src/pages/Users.js
+++ FrontEnd/src/pages/Users.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
+import JSONDATA from '../MOCK_DATA.json';
+import ShowFriends from './showFriends'
 
-function Users() {
+import '../App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Users = () => {
+  const [showFriends, setshowFriends] = useState(JSONDATA)
   return (
     <div className='users'>
-        <h1>This is the page for users</h1>
+        <h2 className="display-1">Number of friends: {showFriends.length} </h2>
+        <ShowFriends showfriends={showFriends}/>
+        <button type='button' className="btn btn-success" onClick={()=>setshowFriends(JSONDATA)}>Get all</button>
+        <button type='button' className="btn btn-success" onClick={()=>setshowFriends([])}>Clear all</button>
       
     </div>
   )
