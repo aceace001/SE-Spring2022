c1c1c30eb788b7b4dc586c456871be6901938ad6
Dafei Du <dud@oregonstate.edu>
2022-04-20 20:07:31 -0400

    Update user_controller.go

--- backend/sprint4/user_controller.go
+++ backend/sprint4/user_controller.go
@@ -1,42 +1,42 @@
-// func ModifyUserInfo(c *gin.Context) {
-// 	var user model.User
-// 	c.ShouldBindJSON(&user)
-// 	log.Logger.Debug("user", log.Any("user", user))
-// 	if err := service.UserService.ModifyUserInfo(&user); err != nil {
-// 		c.JSON(http.StatusOK, response.FailMsg(err.Error()))
-// 		return
-// 	}
+func ModifyUserInfo(c *gin.Context) {
+	var user model.User
+	c.ShouldBindJSON(&user)
+	log.Logger.Debug("user", log.Any("user", user))
+	if err := service.UserService.ModifyUserInfo(&user); err != nil {
+		c.JSON(http.StatusOK, response.FailMsg(err.Error()))
+		return
+	}
 
-// 	c.JSON(http.StatusOK, response.SuccessMsg(nil))
-// }
+	c.JSON(http.StatusOK, response.SuccessMsg(nil))
+}
 
-// func GetUserDetails(c *gin.Context) {
-// 	uuid := c.Param("uuid")
+func GetUserDetails(c *gin.Context) {
+	uuid := c.Param("uuid")
 
-// 	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserDetails(uuid)))
-// }
+	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserDetails(uuid)))
+}
 
-// // Get users information by its name
-// func GetUserOrGroupByName(c *gin.Context) {
-// 	name := c.Query("name")
+// Get users information by its name
+func GetUserOrGroupByName(c *gin.Context) {
+	name := c.Query("name")
 
-// 	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserOrGroupByName(name)))
-// }
+	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserOrGroupByName(name)))
+}
 
-// func GetUserList(c *gin.Context) {
-// 	uuid := c.Query("uuid")
-// 	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserList(uuid)))
-// }
+func GetUserList(c *gin.Context) {
+	uuid := c.Query("uuid")
+	c.JSON(http.StatusOK, response.SuccessMsg(service.UserService.GetUserList(uuid)))
+}
 
-// func AddFriend(c *gin.Context) {
-// 	var userFriendRequest request.FriendRequest
-// 	c.ShouldBindJSON(&userFriendRequest)
+func AddFriend(c *gin.Context) {
+	var userFriendRequest request.FriendRequest
+	c.ShouldBindJSON(&userFriendRequest)
 
-// 	err := service.UserService.AddFriend(&userFriendRequest)
-// 	if nil != err {
-// 		c.JSON(http.StatusOK, response.FailMsg(err.Error()))
-// 		return
-// 	}
+	err := service.UserService.AddFriend(&userFriendRequest)
+	if nil != err {
+		c.JSON(http.StatusOK, response.FailMsg(err.Error()))
+		return
+	}
 
-// 	c.JSON(http.StatusOK, response.SuccessMsg(nil))
-// }
+	c.JSON(http.StatusOK, response.SuccessMsg(nil))
+}
