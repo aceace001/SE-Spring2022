5a2982e2205883979bd4045bcbf1344693c8706d
Yuhao Shi <72894562+aceace001@users.noreply.github.com>
2022-04-20 21:26:29 -0400

    Update Messages.js

--- FrontEnd/src/pages/Messages.js
+++ FrontEnd/src/pages/Messages.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import ChatHistory from '../Components/Chats/ChatHistory';
-import ChatInput from '../Components/ChatInput/ChatInput';
+import ChatHistory from '../Chats/ChatHistory';
+import ChatInput from '../ChatInput/ChatInput';
 import { connect, sendMsg } from '../api';
 import Friends from "../Components/Friends";
 import Online from "../Components/Online";
@@ -37,21 +37,11 @@ class MessageMain extends Component {
 
                 <>
                     <div className="messenger">
-                        <div className="chatMenu">
-                            <div className="chatMenuWrapper">
-                                <input placeholder="Search for friends" className="chatMenuInput"/>
-                                <Friends/>
-                                <Friends/>
-                                <Friends/>
-                                <Friends/>
-                                <Friends/>
 
-                            </div>
-                        </div>
 
                         <div className="chatBox">
                             <div className="currentchatter">
-                                <b>Arthur</b>
+                                <b>Chat History</b>
                             </div>
 
                             <ChatHistory chatHistory={this.state.chatHistory} />
@@ -59,15 +49,7 @@ class MessageMain extends Component {
                             <ChatInput send={this.send} />
 
                         </div>
-                        <div className="chatOnline">
-                            <div className="chatOnlineWrapper">
-                                <Online/>
-                                <Online/>
-                                <Online/>
-                                <Online/>
-                                <Online/>
-                            </div>
-                        </div>
+
                     </div>
                 </>
                 );
